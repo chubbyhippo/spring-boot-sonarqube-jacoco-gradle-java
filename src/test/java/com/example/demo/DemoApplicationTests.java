32d0155cc343f70be8210ebb1f39803ea1424c8c
ch <mkmsacc@outlook.com>
2023-08-13 11:55:00 +0700

    Implement get hello

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient webTestClient;
 
+    @Test
+    void shouldRun() {
+        assertThatNoException().isThrownBy(() -> DemoApplication.main(new String[]{}));
+
+    }
+
+    @Test
+    void shouldReturnHello() {
+        webTestClient.get()
+                .uri("/hello")
+                .exchange()
+                .expectBody(String.class)
+                .value(s -> assertThat(s)
+                        .isEqualTo("Hello"));
+    }
 }
