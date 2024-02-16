80c684803726365da496f73cbe91c14df6a64c71
ch <mkmsacc@outlook.com>
2024-02-16 15:48:43 +0700

    Enhance unit tests in DemoApplicationTests class
    This commit extensively modifies the unit tests in the DemoApplicationTests class, including substantial enhancements and additions. It integrates WebTestClient for testing web endpoints and includes two new test cases: one to check if the application runs without throwing any errors and another to test a basic "/hello" endpoint.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
+    @Autowired
+    private WebTestClient webTestClient;
 
-	@Test
-	void contextLoads() {
-	}
 
+    @Test
+    @DisplayName("should not throw any errors")
+    void shouldNotThrowAnyErrors() {
+        assertThatNoException().isThrownBy(() -> DemoApplication.main(new String[]{"--server.port=0"}));
+    }
+
+    @Test
+    @DisplayName("should return hello")
+    void shouldReturnHello() {
+        webTestClient.get()
+                .uri("/hello")
+                .exchange()
+                .expectBody(String.class)
+                .value(s -> assertThat(s)
+                        .isEqualTo("hello"));
+    }
 }
